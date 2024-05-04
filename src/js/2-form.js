bb021cab351d9d178b7f4b99c50b5518ab3f5e65
Alonka-Le <alyona.sholohowa120@gmail.com>
2024-05-04 12:30:46 +0200

    ///

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,21 @@
-export const form = {};
+const formData = {
+  email: '',
+  message: '',
+};
+
+const emailInput = document.querySelector('#email');
+const messageInput = document.querySelector('#message');
+
+emailInput.addEventListener('input', event => {
+  formData.email = event.target.value;
+  saveFormDataToLocalStorage();
+});
+
+messageInput.addEventListener('input', event => {
+  formData.message = event.target.value;
+  saveFormDataToLocalStorage();
+});
+
+function saveFormDataToLocalStorage() {
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
