a3922993ad487802714949773333acef9570eb7f
Alonka-Le <alyona.sholohowa120@gmail.com>
2024-05-05 23:30:23 +0200

    fix

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,23 +3,39 @@ const formData = {
   message: '',
 };
 
-const emailInput = document.querySelector('.js-form-email');
-const messageInput = document.querySelector('.js-form-message');
-const feedback = document.querySelector('.feedback-form');
+const formRefs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('input[type="email"]'),
+  message: document.querySelector('textarea'),
+};
 
-emailInput.addEventListener('input', event => {
-  formData.email = event.target.value.trim();
-});
-messageInput.addEventListener('input', event => {
-  formData.message = event.target.value.trim();
-});
+populateTextareaField();
 
-feedback.addEventListener('submit', handleSubmit);
+formRefs.form.addEventListener('submit', handleSubmit);
 
-function handleSubmit() {
-  if (formData.email === '' || formData.message === '') {
-    return alert('Fill please all fields');
-  }
-
+formRefs.form.addEventListener('input', () => {
+  formData.email = formRefs.input.value.trim();
+  formData.message = formRefs.message.value.trim();
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+function handleSubmit(event) {
+  event.preventDefault();
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
 }
+
+function populateTextareaField() {
+  const messageText = localStorage.getItem('feedback-form-state');
+  if (messageText) {
+    const parsedData = JSON.parse(messageText);
+    formRefs.input.value = parsedData.email;
+    formRefs.message.value = parsedData.message;
+  }
+}
+
